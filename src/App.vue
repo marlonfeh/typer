ab23caad1ba40ea6f79bf1c2728274f330cdfcb8
marlonfehlau <Marlon.Fehlau@msg.group>
2020-12-24 13:39:10 +0100

    Implements Components for different Sections

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <h2 class="text-pink-600">Test Heading</h2>
-  <p class="underline">Test text</p>
+  <menu-section></menu-section>
+  <stats-section></stats-section>
+  <text-section></text-section>
+  <keyboard-section></keyboard-section> 
 </template>
 
 <script>
+import MenuSection from "./components/menu.vue"
+import StatsSection from "./components/stats.vue"
+import TextSection from "./components/text.vue"
+import KeyboardSection from "./components/keyboard.vue"
+
 export default {
   name: 'App',
   components: {
-
+    MenuSection,
+    StatsSection,
+    TextSection,
+    KeyboardSection
   }
 }
 </script>
---
+++ src/components/keyboard.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <p>Keyboard</p>
+    <div class="border-2 bg-grey-400">
+      <div class="flex space-x-4">
+        <div id="81" class="p-4 border-2 border-gray-700 rounded-md">Q</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">W</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">E</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">R</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">T</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">Z</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">U</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">I</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">O</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">P</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">Ü</div>
+        
+      </div>
+      <div class="ml-2 py-2 flex space-x-4">
+        <div id="81" class="p-4 border-2 border-gray-700 rounded-md">Q</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">W</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">E</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">R</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">T</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">Z</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">U</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">I</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">O</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">P</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">Ü</div>
+      </div>
+      <div class="ml-8 flex space-x-4">
+        <div id="81" class="p-4 border-2 border-gray-700 rounded-md">Q</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">W</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">E</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">R</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">T</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">Z</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">U</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">,</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">.</div>
+        <div class="p-4 border-2 border-gray-700 rounded-md">-</div>
+      </div>
+      <div></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Keyboard',
+}
+</script>
